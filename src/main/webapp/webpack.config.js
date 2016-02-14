4e7ccaca8206fbe4960917a40a433ca51b72be08
lance-r17 <lance.chen.17@gmail.com>
2016-02-14 14:32:55 +0800

    add json loader for webpack and ignore net module loading for browser

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -4,33 +4,31 @@ var webpack = require("webpack");
 module.exports = {
     entry: './app.jsx',
     output: {
-        //path: __dirname + "/../resources/static",
         path: path.join(__dirname, "/../resources/static/site"),
         filename: 'bundle.js'
     },
     module: {
-     loaders: [
-         {
-             test: /\.jsx?$/,
-             loader: 'babel-loader',
-             exclude: /node_modules/,
-             query: {
-                presets: ['react', 'es2015']
-             }
-         },
-         {
-             test: /\.less$/,
-             loader: "style!css!less"
-         }
-     ]
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            },
+            {
+                test: /\.json$/,
+                loader: require.resolve('json-loader')
+            }
+        ]
+    },
+    node: {
+        net: "empty"
     },
     devtool: 'source-map'
-    //resolve: {
-    //    root: [path.join(__dirname, "bower_components")]
-    //},
-    //plugins: [
-    //    new webpack.ResolverPlugin(
-    //        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
-    //    )
-    //]
 }
