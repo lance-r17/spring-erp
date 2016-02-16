51635d81ab6b3573c40db7ad013dc994b78b72e2
lance-r17 <lance.chen.17@gmail.com>
2016-02-16 19:57:02 +0800

    package vendor script and style files with webpack

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -6,19 +6,29 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var config = {
     addVendor: function(name, path) {
         this.resolve.alias[name] = path;
-        this.module.noParse.push(new RegExp('^' + name + '$'));
+        this.module.noParse.push(new RegExp(path));
     },
     context: __dirname,
     entry: {
         bundle: './app.jsx',
-        nifty: './less/nifty.less'
+        'vendors': ['jquery', 'bootstrap'],
+        'vendors.style.min': [ 'bootstrap-style', 'font-awesome'],
+        'react.bundle': ['react', 'react-dom'],
+        'nifty.min': ['nifty-script', 'nifty-style']
     },
     output: {
         path: path.join(__dirname, "/../resources/static/site"),
         filename: '[name].js'
     },
     resolve: {
-        alias: {}
+        alias: {
+            'bootstrap-style': path.resolve(node_modules_dir, 'bootstrap/less/bootstrap.less'),
+            'font-awesome': path.resolve(node_modules_dir, 'font-awesome/less/font-awesome.less'),
+            'react': path.resolve(node_modules_dir, 'react'),
+            'react-dom': path.resolve(node_modules_dir, 'react-dom'),
+            'nifty-script': path.resolve(__dirname, 'nifty/nifty.js'),
+            'nifty-style': path.resolve(__dirname, 'nifty/nifty.less')
+        }
     },
     module: {
         noParse: [],
@@ -29,12 +39,13 @@ var config = {
                 exclude: [node_modules_dir],
                 query: {
                     presets: ['react', 'es2015']
-                }
+                },
             },
-            //{
-            //    test: /\.less$/,
-            //    loader: "style!css!less"
-            //},
+            {
+                // Some legacy modules rely on this being the window object. This becomes a problem when the module is executed in a CommonJS context where this equals module.exports. In this case you can override this with the imports-loader.
+                test: /[\/\\]nifty[\/\\]nifty\.js$/,
+                loader: "imports?this=>window"
+            },
             {
                 test: /\.less$/,
                 loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
@@ -46,14 +57,45 @@ var config = {
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
-            }, {
-                test: /\.(woff|png)$/,
-                loader: 'url-loader?limit=100000'
+            }, 
+            {
+                test: /\.css$/, 
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "file-loader"
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin("[name].css")
+        new webpack.ProvidePlugin({
+            $               : 'jquery',
+            jQuery          : 'jquery',
+            'window.jQuery' : 'jquery'
+        }),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js', Infinity),
+        new ExtractTextPlugin("[name].css"),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
     ],
     node: {
         net: "empty"
@@ -65,34 +107,3 @@ config.addVendor('jquery', path.resolve(node_modules_dir, 'jquery/dist/jquery.mi
 config.addVendor('bootstrap', path.resolve(node_modules_dir, 'bootstrap/dist/js/bootstrap.min.js'));
 
 module.exports = config;
-//module.exports = {
-//    entry: './app.jsx',
-//    output: {
-//        path: path.join(__dirname, "/../resources/static/site"),
-//        filename: 'bundle.js'
-//    },
-//    module: {
-//        loaders: [
-//            {
-//                test: /\.jsx?$/,
-//                loader: 'babel-loader',
-//                exclude: /node_modules/,
-//                query: {
-//                    presets: ['react', 'es2015']
-//                }
-//            },
-//            {
-//                test: /\.less$/,
-//                loader: "style!css!less"
-//            },
-//            {
-//                test: /\.json$/,
-//                loader: require.resolve('json-loader')
-//            }
-//        ]
-//    },
-//    node: {
-//        net: "empty"
-//    },
-//    devtool: 'source-map'
-//}
