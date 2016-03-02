fbec2d64498d8519649e00a6ce821854b8e3b533
lance-r17 <lance.chen.17@gmail.com>
2016-03-02 19:56:34 +0800

    remove obsolete java programs

--- src/main/java/com/sample/erp/QuotationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.erp;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuotationRepository extends JpaRepository<Quotation, Long> {
-	List<Quotation> findByClient(String client);
-}
